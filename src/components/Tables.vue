4a492c24648bcb469049b1615426471679b641ba
Joshua Swiss <swissjoshua@gmail.com>
2017-03-10 10:39:32 +0300

    added tabs for tables

--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -1,7 +1,30 @@
 <!-- tables -->
 <template lang="html">
-  <div id="tables">
-    Table content will go here
+  <div id="tables" class="container is-fluid">
+    <div class="tabs is-centered">
+      <ul>
+        <li class="is-active">
+          <router-link class="nav-item is-tab" to="/tables">
+            Table 1
+          </router-link>
+        </li>
+        <li>
+          <router-link class="nav-item is-tab" to="/tables">
+            Table 2
+          </router-link>
+        </li>
+        <li>
+          <router-link class="nav-item is-tab" to="/tables">
+            Table 3
+          </router-link>
+        </li>
+        <li>
+          <router-link class="nav-item is-tab" to="/tables">
+            Table 4
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script type="text/javascript">
@@ -10,6 +33,8 @@ export default {
 };
 </script>
 
-<style scoped>
-
+<style>
+  .nav-item {
+    margin: 6px 30px 6px 30px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Vuex from 'vuex';
-import axios from 'axios';
 import Home from '@/components/Home';
 import Tables from '@/components/Tables';
 import Dashboard from '@/components/Dashboard';
 
 Vue.use(Router);
 Vue.use(Vuex);
-Vue.use(axios);
 
 const routes = [
   {
