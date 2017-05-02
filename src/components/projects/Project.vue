a3a1a2b94c842cad40bcd2d80d77ecab069f90a0
Joshua Swiss <swissjoshua@gmail.com>
2017-05-02 15:02:53 +0300

    first draft of project page done

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -3,7 +3,7 @@
     <div id="title">
       <h1 id="project-title" class="title is-1"> {{ project['Project title'] }}</h1>
     </div>
-    <div id="basic-info-title">
+    <div class="info-title">
       <h3 class="title is-3">Basic Information</h3>
     </div>
     <div id="basic-info" class="info-box">
@@ -22,128 +22,142 @@
         </div>
       </div>
       <div id="desc" class="columns">
-        <div class="column is-4" id="description">
+        <div class="column is-5" id="description">
           <h4 class="title is-4 is-bold">Description:</h4>
           <p class="desc-text">{{ project['Project objectives / purpose'] || 'n/a' }}</p>
         </div>
+        <div class="column is-2"></div>
+        <div class="column is-5" id="project-website">
+          <h4 class="title is-4">Project Website:</h4>
+          <p class="url">{{ project['Links to project webpages or documents'] || 'n/a'}}</p>
+        </div>
       </div>
     </div>
-    <div id="value" class="columns">
-      <div class="column is-5" id="project-value">
-        <h4 class="title is-4">Total Project Value:</h4>
-        <p class="desc-text">${{ project['Project Value (USD)'] | currency }}</p>
+    <div class="info-title">
+      <h3 class="title is-3">Financial Information</h3>
+    </div>
+    <div id="financial-info" class="info-box">
+      <div id="value" class="columns">
+        <div class="column is-3" id="project-value">
+          <h4 class="title is-4 is-bold">Total Project Value:</h4>
+          <p id="total-value" class="desc-text">${{ project['Project Value (USD)'] | currency }}</p>
+        </div>
+        <div class="column is-3">
+          <h4 class="title is-4 is-bold"><span class="disb">2015 Disbursement:</span></h4>
+          <p class="year-disb">${{ project['2015 Disbursements (USD)'] | currency }}</p>
+        </div>
+        <div class="column is-3">
+          <h4 class="title is-4 is-bold"><span class="disb">2016 Disbursement:</span></h4>
+          <p class="year-disb">${{ project['2016 Disbursements (USD)'] | currency }}</p>
+        </div>
+        <div class="column is-3">
+          <h4 class="title is-4 is-bold"><span class="disb">2017 Disbursement:</span></h4>
+          <p class="year-disb">${{ project['2017 Disbursements (USD)'] | currency }}</p>
+        </div>
+      </div>
+      <div id="table-allocation">
+        <h3 class="title is-4 is-bold">Project Disbursements by Year & Location</h3>
+        <table class="table is-bordered is-striped is-narrow">
+        <thead>
+          <tr>
+            <th><abbr title=""></abbr></th>
+            <th><abbr title="Federal Government of Somalia">FGS</abbr></th>
+            <th><abbr title="Benadir">Benadir</abbr></th>
+            <th><abbr title="2015, USD">Galmudug</abbr></th>
+            <th><abbr title="Hiiraan & Middle Shabelle">HiirShabelle</abbr></th>
+            <th><abbr title="Jubaland">Jubaland</abbr></th>
+            <th><abbr title="Puntland">Puntland</abbr></th>
+            <th><abbr title="Somaliland">Somaliland</abbr></th>
+            <th><abbr title="South West">South West</abbr></th>
+            <th><abbr title="Unattributed">Unattributed</abbr></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="year-row">2015</td>
+            <td>${{ project['FGS - 2015'] || 0 | currency }}</td>
+            <td>${{ project['Benadir - 2015'] || 0 | currency }}</td>
+            <td>${{ project['Galmudug - 2015'] || 0 | currency }}</td>
+            <td>${{ project['Hiiraan & Middle Shabelle - 2015'] || 0 | currency }}</td>
+            <td>${{ project['Jubaland - 2015'] || 0 | currency }}</td>
+            <td>${{ project['Puntland - 2015'] || 0 | currency }}</td>
+            <td>${{ project['Somaliland - 2015'] || 0 | currency }}</td>
+            <td>${{ project['South West - 2015'] || 0 | currency }}</td>
+            <td>${{ project['Unattributed - 2015'] || 0 | currency }}</td>
+          </tr>
+          <tr>
+            <td class="year-row">2016</td>
+            <td>${{ project['FGS - 2016'] || 0 | currency }}</td>
+            <td>${{ project['Benadir - 2016'] || 0 | currency }}</td>
+            <td>${{ project['Galmudug - 2016'] || 0 | currency }}</td>
+            <td>${{ project['Hiiraan & Middle Shabelle - 2016'] || 0 | currency }}</td>
+            <td>${{ project['Jubaland - 2016'] || 0 | currency }}</td>
+            <td>${{ project['Puntland - 2016'] || 0 | currency }}</td>
+            <td>${{ project['Somaliland - 2016'] || 0 | currency }}</td>
+            <td>${{ project['South West - 2016'] || 0 | currency }}</td>
+            <td>${{ project['Unattributed - 2016'] || 0 | currency }}</td>
+          </tr>
+          <tr>
+            <td class="year-row">2017</td>
+            <td>${{ project['FGS - 2017'] || 0 | currency }}</td>
+            <td>${{ project['Benadir - 2017'] || 0 | currency }}</td>
+            <td>${{ project['Galmudug - 2017'] || 0 | currency }}</td>
+            <td>${{ project['Hiiraan & Middle Shabelle - 2017'] || 0 | currency }}</td>
+            <td>${{ project['Jubaland - 2017'] || 0 | currency }}</td>
+            <td>${{ project['Puntland - 2017'] || 0 | currency }}</td>
+            <td>${{ project['Somaliland - 2017'] || 0 | currency }}</td>
+            <td>${{ project['South West - 2017'] || 0 | currency }}</td>
+            <td>${{ project['Unattributed - 2017'] || 0 | currency }}</td>
+          </tr>
+        </tbody>
+      </table>
       </div>
     </div>
-    <div id="disbursements" class="columns has-text-centered">
-      <div class="column is-4">
-        <h4 class="title is-4"><span class="disb">2015 Disbursement:</span></h4>
-        <p class="year-disb">${{ project['2015 Disbursements (USD)'] | currency }}</p>
+    <div class="info-title">
+      <h3 class="title is-3">Funders and Implementers</h3>
+    </div>
+    <div id="basic-info" class="info-box">
+      <div id="funder" class="columns has-text-centered">
+        <div class="column is-5">
+          <h4 class="title is-4 is-bold"><span class="donor">Reporting Agency:</span></h4>
+          <p class="funder">{{ project['Reporting Agency'] || 'n/a' }}</p>
+        </div>
+        <div class="column is-2"></div>
+        <div class="column is-5">
+          <h4 class="title is-4 is-bold"><span class="imp">Implementer(s):</span></h4>
+          <p class="implementer" id="implementer">{{ project['Implementers'] || 'n/a' }}</p>
+        </div>
       </div>
-      <div class="column is-4">
-        <h4 class="title is-4"><span class="disb">2016 Disbursement:</span></h4>
-        <p class="year-disb">${{ project['2016 Disbursements (USD)'] | currency }}</p>
-      </div>
-      <div class="column is-4">
-        <h4 class="title is-4"><span class="disb">2017 Disbursement:</span></h4>
-        <p class="year-disb">${{ project['2017 Disbursements (USD)'] | currency }}</p>
+      <div id="implementer" class="columns has-text-centered">
+        <div class="column is-5">
+          <h4 class="title is-4 is-bold"><span class="donor">Funder(s):</span></h4>
+          <p class="funder">{{ project['Funders'] || 'n/a' }}</p>
+        </div>
+        <div class="column is-2"></div>
+        <div class="column is-5">
+          <h4 class="title is-4 is-bold"><span class="imp">Implementer Category:</span></h4>
+          <p class="implementer" id="imp-category">{{ project['Implementer Category'] || 'n/a' }}</p>
+        </div>
       </div>
     </div>
-    <div id="funder" class="columns has-text-centered">
-      <div class="column is-6">
-        <h4 class="title is-4"><span class="donor">Reporting Agency:</span></h4>
-        <p class="funder">{{ project['Reporting Agency'] || 'n/a' }}</p>
-      </div>
-      <div class="column is-6">
-        <h4 class="title is-4"><span class="donor">Funder(s):</span></h4>
-        <p class="funder">{{ project['Funders'] || 'n/a' }}</p>
-      </div>
+    <div class="info-title">
+      <h3 class="title is-3">Project Thematic Focuses</h3>
     </div>
-    <div id="implementer" class="columns has-text-centered">
-      <div class="column is-6">
-        <h4 class="title is-4"><span class="imp">Implementer(s):</span></h4>
-        <p class="implementer" id="implementer">{{ project['Implementers'] || 'n/a' }}</p>
+    <div id="basic-info" class="info-box">
+      <div id="focuses" class="columns has-text-centered">
+        <div class="column is-4">
+          <h4 class="title is-4 is-bold"><span class="focus">Gender:</span></h4>
+          <p class="proj-focus">{{ project.Gender || 'n/a' }}</p>
+        </div>
+        <div class="column is-4">
+          <h4 class="title is-4 is-bold"><span class="focus">Capacity Building:</span></h4>
+          <p class="proj-focus">{{ project['Capacity building'] || 'n/a' }}</p>
+        </div>
+        <div class="column is-4">
+          <h4 class="title is-4 is-bold"><span class="focus">Stabilization:</span></h4>
+          <p class="proj-focus">{{ project.Stabilization || 'n/a' }}</p>
+        </div>
       </div>
-      <div class="column is-6">
-        <h4 class="title is-4"><span class="imp">Implementer Category:</span></h4>
-        <p class="implementer" id="imp-category">{{ project['Implementer Category'] || 'n/a' }}</p>
-      </div>
-    </div>
-    <div id="website" class="columns">
-      <div class="column is-5" id="project-website">
-        <h4 class="title is-4">Project Website:</h4>
-        <p class="url">{{ project['Links to project webpages or documents'] || 'n/a'}}</p>
-      </div>
-    </div>
-    <div id="focuses" class="columns has-text-centered">
-      <div class="column is-4">
-        <h4 class="title is-4"><span class="focus">Gender:</span></h4>
-        <p class="proj-focus">{{ project.Gender || 'n/a' }}</p>
-      </div>
-      <div class="column is-4">
-        <h4 class="title is-4"><span class="focus">Capacity Building:</span></h4>
-        <p class="proj-focus">{{ project['Capacity building'] || 'n/a' }}</p>
-      </div>
-      <div class="column is-4">
-        <h4 class="title is-4"><span class="focus">Stabilization:</span></h4>
-        <p class="proj-focus">{{ project.Stabilization || 'n/a' }}</p>
-      </div>
-    </div>
-    <div id="table-allocation">
-      <h3 class="title is-3">Project Disbursements by Year & Location</h3>
-      <table class="table is-bordered is-striped is-narrow">
-      <thead>
-        <tr>
-          <th><abbr title=""></abbr></th>
-          <th><abbr title="Federal Government of Somalia">FGS</abbr></th>
-          <th><abbr title="Benadir">Benadir</abbr></th>
-          <th><abbr title="2015, USD">Galmudug</abbr></th>
-          <th><abbr title="Hiiraan & Middle Shabelle">HiirShabelle</abbr></th>
-          <th><abbr title="Jubaland">Jubaland</abbr></th>
-          <th><abbr title="Puntland">Puntland</abbr></th>
-          <th><abbr title="Somaliland">Somaliland</abbr></th>
-          <th><abbr title="South West">South West</abbr></th>
-          <th><abbr title="Unattributed">Unattributed</abbr></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="year-row">2015</td>
-          <td>${{ project['FGS - 2015'] || 0 | currency }}</td>
-          <td>${{ project['Benadir - 2015'] || 0 | currency }}</td>
-          <td>${{ project['Galmudug - 2015'] || 0 | currency }}</td>
-          <td>${{ project['Hiiraan & Middle Shabelle - 2015'] || 0 | currency }}</td>
-          <td>${{ project['Jubaland - 2015'] || 0 | currency }}</td>
-          <td>${{ project['Puntland - 2015'] || 0 | currency }}</td>
-          <td>${{ project['Somaliland - 2015'] || 0 | currency }}</td>
-          <td>${{ project['South West - 2015'] || 0 | currency }}</td>
-          <td>${{ project['Unattributed - 2015'] || 0 | currency }}</td>
-        </tr>
-        <tr>
-          <td class="year-row">2016</td>
-          <td>${{ project['FGS - 2016'] || 0 | currency }}</td>
-          <td>${{ project['Benadir - 2016'] || 0 | currency }}</td>
-          <td>${{ project['Galmudug - 2016'] || 0 | currency }}</td>
-          <td>${{ project['Hiiraan & Middle Shabelle - 2016'] || 0 | currency }}</td>
-          <td>${{ project['Jubaland - 2016'] || 0 | currency }}</td>
-          <td>${{ project['Puntland - 2016'] || 0 | currency }}</td>
-          <td>${{ project['Somaliland - 2016'] || 0 | currency }}</td>
-          <td>${{ project['South West - 2016'] || 0 | currency }}</td>
-          <td>${{ project['Unattributed - 2016'] || 0 | currency }}</td>
-        </tr>
-        <tr>
-          <td class="year-row">2017</td>
-          <td>${{ project['FGS - 2017'] || 0 | currency }}</td>
-          <td>${{ project['Benadir - 2017'] || 0 | currency }}</td>
-          <td>${{ project['Galmudug - 2017'] || 0 | currency }}</td>
-          <td>${{ project['Hiiraan & Middle Shabelle - 2017'] || 0 | currency }}</td>
-          <td>${{ project['Jubaland - 2017'] || 0 | currency }}</td>
-          <td>${{ project['Puntland - 2017'] || 0 | currency }}</td>
-          <td>${{ project['Somaliland - 2017'] || 0 | currency }}</td>
-          <td>${{ project['South West - 2017'] || 0 | currency }}</td>
-          <td>${{ project['Unattributed - 2017'] || 0 | currency }}</td>
-        </tr>
-      </tbody>
-    </table>
     </div>
   </div>
 </template>
@@ -187,6 +201,7 @@
   #pillars {
     margin-top: 0%;
   }
+
   .title {
     margin-bottom: 2px;
   }
@@ -208,7 +223,13 @@
     padding: 0% 2% 1% 2%;
     box-shadow: 1px 1px 3px #c6c6c6;
   }
-  #basic-info-title {
+  .info-title {
     margin-top: 2%;
+  }
+  #total-value {
+    font-weight: bold;
+  }
+  #value {
+    margin-top: 1%;
   }
 </style>
