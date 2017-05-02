4d73e0e11e7453cea9f85c3c2b09a68dd8fcd6c4
Joshua Swiss <swissjoshua@gmail.com>
2017-05-02 14:39:41 +0300

    grouping box done for project profile

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -1,89 +1,90 @@
 <template lang="html">
   <div class="container">
-    <section class="hero is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 id="project-title" class="title is-1"> {{ project['Project title'] }}</h1>
+    <div id="title">
+      <h1 id="project-title" class="title is-1"> {{ project['Project title'] }}</h1>
+    </div>
+    <div id="basic-info-title">
+      <h3 class="title is-3">Basic Information</h3>
+    </div>
+    <div id="basic-info" class="info-box">
+      <div id="pillars" class="columns">
+        <div class="column is-4">
+          <h4 class="title is-4 is-bold"><span class="ndp">NDP Pillar:</span></h4>
+          <p class="pillar">{{ project['NDP Pillar'] }}</p>
+        </div>
+        <div class="column is-4">
+          <h4 class="title is-4 is-bold"><span class="ndp">Primary Sector:</span></h4>
+          <p class="pillar">{{ project['Primary Sector'] }}</p>
+        </div>
+        <div class="column is-4">
+          <h4 class="title is-4 is-bold"><span class="ndp">Related Sectors:</span></h4>
+          <p class="pillar">{{ project['Links to other Sector'] || 'n/a' }}</p>
         </div>
       </div>
-    </section>
-    <div id="pillars" class="columns">
-      <div class="column is-4">
-        <h3 class="title is-4"><span class="ndp">NDP Pillar:</span></h3>
-        <p id="pillar">{{ project['NDP Pillar'] }}</p>
-      </div>
-      <div class="column is-4">
-        <h3 class="title is-4"><span class="ndp">Primary Sector:</span></h3>
-        <p id="pillar">{{ project['Primary Sector'] }}</p>
-      </div>
-      <div class="column is-4">
-        <h3 class="title is-4"><span class="ndp">Related Sectors:</span></h3>
-        <p id="pillar">{{ project['Links to other Sector'] || 'n/a' }}</p>
-      </div>
-    </div>
-    <div id="desc" class="columns">
-      <div class="column is-5" id="description">
-        <h3 class="title is-4">Description:</h3>
-        <p class="desc-text">{{ project['Project objectives / purpose'] || 'n/a' }}</p>
+      <div id="desc" class="columns">
+        <div class="column is-4" id="description">
+          <h4 class="title is-4 is-bold">Description:</h4>
+          <p class="desc-text">{{ project['Project objectives / purpose'] || 'n/a' }}</p>
+        </div>
       </div>
     </div>
     <div id="value" class="columns">
       <div class="column is-5" id="project-value">
-        <h3 class="title is-4">Total Project Value:</h3>
+        <h4 class="title is-4">Total Project Value:</h4>
         <p class="desc-text">${{ project['Project Value (USD)'] | currency }}</p>
       </div>
     </div>
     <div id="disbursements" class="columns has-text-centered">
       <div class="column is-4">
-        <h3 class="title is-4"><span class="disb">2015 Disbursement:</span></h3>
+        <h4 class="title is-4"><span class="disb">2015 Disbursement:</span></h4>
         <p class="year-disb">${{ project['2015 Disbursements (USD)'] | currency }}</p>
       </div>
       <div class="column is-4">
-        <h3 class="title is-4"><span class="disb">2016 Disbursement:</span></h3>
+        <h4 class="title is-4"><span class="disb">2016 Disbursement:</span></h4>
         <p class="year-disb">${{ project['2016 Disbursements (USD)'] | currency }}</p>
       </div>
       <div class="column is-4">
-        <h3 class="title is-4"><span class="disb">2017 Disbursement:</span></h3>
+        <h4 class="title is-4"><span class="disb">2017 Disbursement:</span></h4>
         <p class="year-disb">${{ project['2017 Disbursements (USD)'] | currency }}</p>
       </div>
     </div>
     <div id="funder" class="columns has-text-centered">
       <div class="column is-6">
-        <h3 class="title is-4"><span class="donor">Reporting Agency:</span></h3>
+        <h4 class="title is-4"><span class="donor">Reporting Agency:</span></h4>
         <p class="funder">{{ project['Reporting Agency'] || 'n/a' }}</p>
       </div>
       <div class="column is-6">
-        <h3 class="title is-4"><span class="donor">Funder(s):</span></h3>
+        <h4 class="title is-4"><span class="donor">Funder(s):</span></h4>
         <p class="funder">{{ project['Funders'] || 'n/a' }}</p>
       </div>
     </div>
     <div id="implementer" class="columns has-text-centered">
       <div class="column is-6">
-        <h3 class="title is-4"><span class="imp">Implementer(s):</span></h3>
+        <h4 class="title is-4"><span class="imp">Implementer(s):</span></h4>
         <p class="implementer" id="implementer">{{ project['Implementers'] || 'n/a' }}</p>
       </div>
       <div class="column is-6">
-        <h3 class="title is-4"><span class="imp">Implementer Category:</span></h3>
+        <h4 class="title is-4"><span class="imp">Implementer Category:</span></h4>
         <p class="implementer" id="imp-category">{{ project['Implementer Category'] || 'n/a' }}</p>
       </div>
     </div>
     <div id="website" class="columns">
       <div class="column is-5" id="project-website">
-        <h3 class="title is-4">Project Website:</h3>
+        <h4 class="title is-4">Project Website:</h4>
         <p class="url">{{ project['Links to project webpages or documents'] || 'n/a'}}</p>
       </div>
     </div>
     <div id="focuses" class="columns has-text-centered">
       <div class="column is-4">
-        <h3 class="title is-4"><span class="focus">Gender:</span></h3>
+        <h4 class="title is-4"><span class="focus">Gender:</span></h4>
         <p class="proj-focus">{{ project.Gender || 'n/a' }}</p>
       </div>
       <div class="column is-4">
-        <h3 class="title is-4"><span class="focus">Capacity Building:</span></h3>
+        <h4 class="title is-4"><span class="focus">Capacity Building:</span></h4>
         <p class="proj-focus">{{ project['Capacity building'] || 'n/a' }}</p>
       </div>
       <div class="column is-4">
-        <h3 class="title is-4"><span class="focus">Stabilization:</span></h3>
+        <h4 class="title is-4"><span class="focus">Stabilization:</span></h4>
         <p class="proj-focus">{{ project.Stabilization || 'n/a' }}</p>
       </div>
     </div>
@@ -184,7 +185,7 @@
 
 <style scoped>
   #pillars {
-    margin-top: 3%;
+    margin-top: 0%;
   }
   .title {
     margin-bottom: 2px;
@@ -199,6 +200,15 @@
     font-weight: bold;
   }
   #table-allocation {
+    margin-top: 2%;
+  }
+  .info-box {
+    border: 1px solid #8D8D8D;
+    margin: 2%;
+    padding: 0% 2% 1% 2%;
+    box-shadow: 1px 1px 3px #c6c6c6;
+  }
+  #basic-info-title {
     margin-top: 2%;
   }
 </style>
