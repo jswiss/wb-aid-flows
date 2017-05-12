d9c00c31d1ec86e25a9ee0b1992254cae51b73cd
Joshua Swiss <swissjoshua@gmail.com>
2017-05-12 07:47:36 +0300

    added google analytics

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -44,6 +44,16 @@ import TreeMap2015 from './treemap/TreeMap2015';
 import TreeMap2016 from './treemap/TreeMap2016';
 import TreeMap2017 from './treemap/TreeMap2017';
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-99055406-1', 'auto');
+  ga('send', 'pageview');
+
+
+
 export default {
   name: 'home',
   components: {
